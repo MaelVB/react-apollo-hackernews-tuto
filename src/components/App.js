4bfec06fda2ed0ddfe6958aa6bdd0c061a026751
MaelVB <vaillant.beuchot.mael@gmail.com>
2020-10-29 16:09:58 +0100

    [new] Add Links creation, Routing and Authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
+import React, { Component } from 'react'
+import LinkList from './LinkList'
+import CreateLink from './CreateLink'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
+import Login from './Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,20 +8,39 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import { AUTH_TOKEN } from './constants'
+
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+// Middleware to put authorization token in each request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
