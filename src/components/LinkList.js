383c070eb372ee3ef1f4d2cc60f468a31f8c9c92
MaelVB <vaillant.beuchot.mael@gmail.com>
2020-10-29 17:55:34 +0100

    [new] Add upvotes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,16 +4,26 @@ import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
 const FEED_QUERY = gql`
-  {
-    feed {
-      links {
-        id
-        createdAt
-        url
-        description
-      }
+    {
+        feed {
+            links {
+                    id
+                    createdAt
+                    url
+                    description
+                    postedBy {
+                    id
+                    name
+                    }
+                    votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+        }
     }
-  }
 `
 
 class LinkList extends Component {
@@ -28,7 +38,9 @@ class LinkList extends Component {
     
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+                {linksToRender.map((link, index) => (
+                    <Link key={link.id} link={link} index={index} />
+                ))}
             </div>
           )
         }}
