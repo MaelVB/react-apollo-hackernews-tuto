fb150582229d38c06e4022c82df35c45b580f47f
MaelVB <vaillant.beuchot.mael@gmail.com>
2020-11-02 11:59:22 +0100

    [new] Add search bar

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import CreateLink from './CreateLink'
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
 import Login from './Login'
+import Search from './Search'
+
 
 function App() {
   return (
@@ -11,9 +13,10 @@ function App() {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Switch>
-          <Route exact path="/" component={LinkList} />
-          <Route exact path="/create" component={CreateLink} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path='/' component={LinkList} />
+          <Route exact path='/create' component={CreateLink} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/search' component={Search} />
         </Switch>
       </div>
     </div>
