3314a8ec8b68df799c3aa3676028625bbca7e149
MaelVB <vaillant.beuchot.mael@gmail.com>
2020-11-02 10:04:41 +0100

    [new] Add automatic updates

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import { FEED_QUERY } from './LinkList'
+
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -43,7 +45,16 @@ class CreateLink extends Component {
         <Mutation
             mutation={POST_MUTATION}
             variables={{ description, url }}
-            onCompleted={() => this.props.history.push('/')}>
+            onCompleted={() => this.props.history.push('/')}
+            update={(store, { data: { post } }) => {
+              const data = store.readQuery({ query: FEED_QUERY })
+              data.feed.links.unshift(post)
+              store.writeQuery({
+                query: FEED_QUERY,
+                data
+              })
+            }}
+        >
                 
             {postMutation => <button onClick={postMutation}>Submit</button>}
         </Mutation>
