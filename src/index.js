f6718d78832b4b2ef26082eb566c176f95655c19
MaelVB <vaillant.beuchot.mael@gmail.com>
2020-11-03 22:58:56 +0100

    [new] websocket implentation

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { AUTH_TOKEN } from './constants'
 
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
@@ -30,8 +34,28 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+// WebSocket
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
